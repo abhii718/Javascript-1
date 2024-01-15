4c3cab7b285ca32bb9d9f77bb18a74463d33f179
abhii718 <abhinaysingh47639@gamil.com>
2024-01-15 19:54:22 +0530

    a

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -7,7 +7,7 @@ const myHeros = ["shaktiman", " abhitab"];
 const myArry2 = new Array(1, 2, 3, 4, 5);
 console.log(myArr[0]);
 
-// *********  Array methods ************6
+// *********  Array methods ************
 
 
 /*

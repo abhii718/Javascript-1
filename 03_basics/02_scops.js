14b9c295c66dd8e8fcfb0d008dfaf1aaee2a1fbd
abhii718 <abhinaysingh47639@gamil.com>
2024-01-16 10:36:25 +0530

    scops

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,15 @@
+// var c = 300;
+let a = 300;
+if (true) {
+  let a = 10;
+  const b = 20;
+  //   c = 30;
+  //   var c = 38;
+  console.log("Inner", a)
+
+
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c);

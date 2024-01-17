7a012a2c42823e444bb23c0770ca4c5d9276fc1b
abhii718 <abhinaysingh47639@gamil.com>
2024-01-17 10:07:35 +0530

    scope

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -5,11 +5,46 @@ if (true) {
   const b = 20;
   //   c = 30;
   //   var c = 38;
-  console.log("Inner", a)
-
-
+  console.log("Inner", a);
 }
 
-console.log(a)
+console.log(a);
 // console.log(b)
 // console.log(c);
+
+// nested scope
+
+function one() {
+  const username = "hitesh ";
+
+  function two() {
+    const website = " youtube ";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+
+one();
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+  // console.log(website); scope base defind
+}
+// console.log(username); Innerscope
+
+// ++++++++++++interesting +++++++++++++
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+const addTwo = function (num) {
+  return num + 2;
+};
+console.log(addTwo(5));
+ 

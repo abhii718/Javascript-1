26600f5c040c6fbc8068cb27bca9b20edfc92bcc
abhii718 <abhinaysingh47639@gamil.com>
2024-01-20 16:11:54 +0530

    for each loops

---
+++ 05_Iterations/six.js
@@ -0,0 +1,14 @@
+// const coding = ["1js", "ruby", "java", "python"];
+
+// const value = coding.forEach( (item ) => {
+//     console.log(item)
+//     return item;
+// })
+// console.log(value) 
+
+
+const myNums = [ 1,2,3,4,5,6,7,8]
+
+const newNums= myNums.filter( (  num )=> num> 4 )
+
+console.log(newNums)

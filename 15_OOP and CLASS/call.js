0df734b779f633c3ae492e6ade5ad8ba55af493e
abhii718 <abhinaysingh47639@gamil.com>
2024-01-28 13:01:09 +0530

    call

---
+++ 15_OOP and CLASS/call.js
@@ -0,0 +1,14 @@
+function SetUsername(username) {
+  // complex DB calls
+  this.username = username;
+  console.log("called");
+}
+function createUser(username, email, password) {
+  SetUsername.call(this, username);
+
+  this.email = email;
+  this.password = password;
+}
+
+const chai = new createUser("chai", "chai@b.com", "123");
+console.log(chai);
